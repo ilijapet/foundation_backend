7172b23a7da630a3306657ee064c17cba4f7b1a1
ilija <ilijapet@gmail.com>
2024-01-25 17:37:04 +0100

    update

--- foundation_backend/project/settings/base.py
+++ foundation_backend/project/settings/base.py
@@ -21,6 +21,7 @@ if os.path.isfile(env_file):
     env.read_env(env_file)
     POSTGRES_PASSWORD = env("POSTGRES_PASSWORD")
     ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
+    print("ALLOWED", ALLOWED_HOSTS)
 else:
     raise ValueError("We cannot find .env file")
 
